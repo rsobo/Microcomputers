d521bac0da05dd5903393a4441149841cd8135df
Ryan Sobolewski <ryansobolewski2000@gmail.com>
2022-02-14 08:25:48 -0500

    Update and rename Prelab2 to Prelab2.asm

--- Prelab2.asm
+++ Prelab2.asm
@@ -9,16 +9,16 @@
 ; Replace with your application code
 start:
 ;Q1
-;   LDI R16, $92
-;	LDI R17, $23
-;	LDI R18, $66
-;	LDI R19, $87
-;	LDI R20, $F5
-;	ADD R16, R17
-;	ADD R18, R19
-;	ADD R16, R18
-;	ADD R16, R20
-;
+        LDI R16, $92
+	LDI R17, $23
+	LDI R18, $66
+	LDI R19, $87
+	LDI R20, $F5
+	ADD R16, R17
+	ADD R18, R19
+	ADD R16, R18
+	ADD R16, R20
+
 ;Q2
 	LDI R16, 0b11111111
 	LDI R17, 0b00000000
